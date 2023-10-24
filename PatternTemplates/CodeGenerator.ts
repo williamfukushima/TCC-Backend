1199b32652d7109fdd2882db111c575e95930c40
William Fukushima <51289763+williamfukushima@users.noreply.github.com>
2023-10-24 01:21:31 -0300

    change download template route to create file and download

--- PatternTemplates/CodeGenerator.ts
+++ PatternTemplates/CodeGenerator.ts
@@ -1,14 +1,15 @@
 import fs from "fs";
+import path from "path";
 
 class TemplateGenerator {
   pascalCaseRegex = /^[A-Z][a-z]+(?:[A-Z][a-z]+)*$/;
   validFileRegex = /[a-zA-Z0-9_]/;
   classRegex = /(class)/;
 
-  constructor() {}
-  createFile(templatePath: string, destinationPath: string, className: string) {
+  constructor() { }
+  async createFile(templatePath: string, destinationPath: string, className: string): Promise<void> {
     const fileContent = this.readTemplate(templatePath, className);
-    
+
     fs.writeFile(destinationPath, fileContent, (err: any) => {
       if (err) {
         console.error(`Error writing to ${destinationPath}: ${err}`);
@@ -20,28 +21,38 @@ class TemplateGenerator {
 
   readTemplate(path: string, className: string): string {
     const fileContent = fs.readFileSync(path, "utf-8");
-    const lines = fileContent.split("\n"); 
+    const lines = fileContent.split("\n");
     var modifiedFileContent = "";
 
     // Change content to match user specs
-    
+
     for (const line of lines) {
       var newLine = line;
       {
         // Change class name
 
         var matches = /class (.*?) /g.exec(line);
-        if (matches != null && matches.length > 1)
-        {
-          if(matches[1])
-          {
+        if (matches != null && matches.length > 1) {
+          if (matches[1]) {
             newLine = line.replace(matches[1], className);
           }
         }
         modifiedFileContent += newLine + "\n";
-      } 
+      }
     }
     return modifiedFileContent;
+  }
+
+  deleteFile(filepath: string) {
+    if (this.validFileRegex.test(filepath)) {
+      fs.rm(filepath, (err: any) => {
+        if (err) {
+          console.error(`Error deleting from ${filepath}: ${err}`);
+        } else {
+          console.log(`Successfully deleted file ${filepath}`);
+        }
+      })
+    }
   }
 }
 
--- routes/template.route.ts
+++ routes/template.route.ts
@@ -1,14 +1,23 @@
-import path from "path";
 import TemplateGenerator from "../PatternTemplates/CodeGenerator";
+import { Request, Response } from "express";
+import fs from "fs";
 
-const createTemplateRoute = (req: any, res: any) => {
+import archiver from 'archiver';
+
+const createTemplateRoute = async (req: Request, res: Response) => {
   const classname = req.query.classname;
   const template = req.query.template;
-  const filePath = path.join(__dirname, `${classname}.ts`);
+  const filePath = `public/${classname}.ts`;
 
   const app = new TemplateGenerator();
-  app.createFile(`PatternTemplates/${template}/${template}.ts`, `public/${classname}.ts`,`${classname}`)
-  res.status(200);
-  res.send(200, "Successfully created and wrote to ObservableTest.ts");
+  await app.createFile(`PatternTemplates/${template}/${template}.ts`, filePath,`${classname}`);
+
+  res.download(filePath, classname + ".ts", (err: Error) => {
+      res.status(200);
+      if (err) {
+        res.status(500).send("Internal Server Error");
+      }
+    });
+  app.deleteFile(filePath);
 }
 export default createTemplateRoute;
